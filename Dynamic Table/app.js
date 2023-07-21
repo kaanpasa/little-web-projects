dee538ad2974906bfb83a4617b38d741035a80f8
Kaan Pasa <45922962+kaanpasa@users.noreply.github.com>
2023-07-22 00:43:16 +0300

    Added Dynamic Table

---
+++ Dynamic Table/app.js
@@ -0,0 +1,24 @@
+var table = document.getElementById("table"),rIndex;
+            
+for(var i = 1; i < table.rows.length; i++){
+    table.rows[i].onclick = function(){
+        rIndex = this.rowIndex;
+        console.log(rIndex);
+
+        document.getElementById("fname").value = this.cells[1].innerHTML;
+        document.getElementById("lname").value = this.cells[2].innerHTML;
+        document.getElementById("country").value = this.cells[3].innerHTML;
+        document.getElementById("mNumber").value = this.cells[4].innerHTML;
+    };
+}
+            
+function editRow(){
+    table.rows[rIndex].cells[1].innerHTML = document.getElementById("fname").value;
+    table.rows[rIndex].cells[2].innerHTML = document.getElementById("lname").value;
+    table.rows[rIndex].cells[3].innerHTML = document.getElementById("country").value;
+    table.rows[rIndex].cells[4].innerHTML = document.getElementById("mNumber").value;
+}
+
+function editTableDisplay(){
+    document.querySelector('.editTable').setAttribute('style', 'display: block;')
+}
